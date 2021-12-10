180f96c75be9531b6dae3cbf82dd0a2f84a79fb4
Enzo Berther <EBerther@hamilton-medical.com>
2021-12-10 11:08:17 +0100

    Add project files.

---
+++ SyncfusionLinearGaugeTest/SyncfusionLinearGaugeTest/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SyncfusionLinearGaugeTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
